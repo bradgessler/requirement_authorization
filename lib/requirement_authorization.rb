5bd47d2bfdbd16a301f1be3605a3c5b32bab3e04
Benjamin Fleischer <github@benjaminfleischer.com>
2014-02-05 14:54:02 -0600

    Correct method signature of eval'd class method from call to resolve

--- lib/requirement_authorization.rb
+++ lib/requirement_authorization.rb
@@ -2,18 +2,18 @@ require "requirement_authorization/version"
 
 module RequirementAuthorization
   METHOD_SUFIX = '_required'
-  
+
   def self.included(base)
     base.send :extend, ClassMethods
   end
-  
+
   class Requirement
     CONTROLLER_OPTIONS = [:only, :except, :if, :unless]
 
     def initialize(opts={},&block)
       yield self if block_given?
     end
-    
+
     # A proc or method that must return a true for the requirement to be satisified
     def guard(proc=nil, &block)
       @guard = controller_filter_proc(proc, &block)
@@ -24,28 +24,24 @@ module RequirementAuthorization
       # Lazily invert the return of the controller proc.
       @guard = Proc.new{|c, args| not controller_filter_proc(proc, &block).call(c, *args)}
     end
-    
+
     # This is the method that we'll call if the guard fails. The resolution should take the user
     # through a flow where they can satisify the requirements for the requirement and pass on through.
     def resolution(proc=nil, &block)
       @resolution = controller_filter_proc(proc, &block)
     end
-    
+
     # Sets up the filter for the controller by wrapping this requirement up in a proc.
     def filter(controller, *args)
       args, controller_options = extract_filter_args!(args)
-      # Rails 3 does something `self` in the controller block, so 
-      # I create the `requirement` reference and pass it into the controller block
-      # for resolution.
-      requirement = self
-      controller.before_filter(controller_options){ |c| requirement.resolve(c, *args) }
+      controller.before_filter(controller_options){ |c| resolve(c, *args) }
     end
-    
+
     # The gaurd, resolution process. This is where the magic happens.
     def resolve(controller_instance, *args)
       @resolution.call(controller_instance, *args) if @guard.call(controller_instance, *args)
     end
-    
+
   protected
     # Packages up a proc or a block into something that a controller can deal with
     def controller_filter_proc(proc=nil, &block)
@@ -76,29 +72,32 @@ module RequirementAuthorization
       [ args, controller_options ]
     end
   end
-  
+
   module ClassMethods
     # Setups a hash table of requirements that may be used by class methods from
     # other sub-classed controllers
     def requirement(requirement, opts={}, &block)
       self.requirements.merge! requirement.to_s => Requirement.new(opts, &block)
-      
+
       # Build out the class method for this requirement. This is primarly used towards the
       # top of a controller.
-      self.class.class_eval %{
+      instance_eval %{
         def #{requirement}#{METHOD_SUFIX}(*args)
           requirements['#{requirement}'].filter(self, *args)
         end}
-      
+
       # Build out the instance method so that this requirement can be called from other
-      # instance methods. This proves to be insanely useful for composing requirements 
+      # instance methods. This proves to be insanely useful for composing requirements
       # together or reusing them from other methods.
-      self.class_eval %{
+      class_eval %{
         def #{requirement}#{METHOD_SUFIX}(*args)
-          self.class.send(:requirements)['#{requirement}'].call(self, *args)
+          req = '#{requirement}'
+          self.class.send(:requirements).
+            fetch(req).
+            resolve(self, *args)
         end}
     end
-        
+
   protected
     def requirements
       @@requirements ||= {}
